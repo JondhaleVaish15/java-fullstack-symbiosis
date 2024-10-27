f04bcbfa13544ec3e0a90a53a9f63579d0bdacde
JondhaleVaish15 <vaishnavijondhale34@gmail.com>
2024-10-27 11:32:22 +0530

    Add files via upload

---
+++ DBUtil.java
@@ -0,0 +1,30 @@
+package jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.*;
+public class DBUtil{
+	
+	private static Connection Can;
+	private static final String DB_DRIVER_CLASS="com.mysql.cj.jdbc.Driver";
+	private static final String DB_USERNAME="root";
+	private static final String DB_PASSWORD="mysql8";
+	private static final String DB_URL="jdbc:mysql://localhost:3306/bank13";
+	public static Connection getConnection() {
+		try {
+			Class.forName( DB_DRIVER_CLASS);
+			System.out.println("Sucess");
+			Can=DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+			System.out.println("Collection");
+		}catch(ClassNotFoundException e) {
+			System.out.println("error"+e.getMessage());
+		}
+		catch(SQLException e) {
+			System.out.println("error"+e.getMessage());
+		}
+	
+          return Can;
+}
+}
+
+
